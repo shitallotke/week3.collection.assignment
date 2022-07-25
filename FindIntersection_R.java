014c738c445901fa6e9b1cbc715365b367c947e4
shitallotke <108998809+shitallotke@users.noreply.github.com>
2022-07-25 15:06:34 -0500

    Add files via upload

---
+++ FindIntersection_R.java
@@ -0,0 +1,34 @@
+package week3.day2.collectionassignment;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindIntersection_R {
+
+	public static void main(String[] args) {
+		Integer[] arr1 =	{3,2,11,4,6,7};	 
+		Integer[] arr2 =   {1,2,8,4,9,7};
+	
+	   Set<Integer> set1 = new HashSet<Integer>();
+	   Set<Integer> set2 = new HashSet<Integer>();
+	   
+	   set1.addAll(Arrays.asList(arr1));
+	   System.out.print("1st set : ");
+	   System.out.print(set1);
+	   
+	   set2.addAll(Arrays.asList(arr2));
+	   System.out.print("\n"+"2nd set : ");
+	   System.out.print(set2);
+	   
+	   System.out.print("\n"+"Intersection of two sets : ");
+	   set1.retainAll(set2);
+	   System.out.print(set1);
+	}
+
+}
+/*output:
+ 1st set : [2, 3, 4, 6, 7, 11]
+2nd set : [1, 2, 4, 7, 8, 9]
+Intersection of two sets : [2, 4, 7]
+*/
